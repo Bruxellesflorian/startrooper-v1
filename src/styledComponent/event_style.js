9394ce369c38f5a6c9b32d291d91d6dca50b32ac
bruxellesflorian <florianbertchi@gmail.com>
2022-02-04 17:34:25 +0100

    scale font event mobile

--- src/styledComponent/event_style.js
+++ src/styledComponent/event_style.js
@@ -2,40 +2,34 @@ import styled from 'styled-components'
 
 export const Display_all_event = styled.div`
   @media (max-width: 735px) {
-      div{
-    font-size: 12px;
-    padding: 1%;
-
-}
-
+    div {
+      font-size: 12px;
+      padding: 1%;
+    }
   }
 
-    position: absolute;
-    top: 0;
-    width: 100%;
-    >div{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        height: 100vh;
-    }
+  position: absolute;
+  top: 0;
+  width: 100%;
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+  }
 `
 
 export const Story_box = styled.div`
-    width: fit-content;
-    text-align: center;
-    margin: 0 auto;
-    font-size: 30px;
-
+  width: fit-content;
+  text-align: center;
+  margin: 0 auto;
+  font-size: 30px;
 `
 export const Question = styled.div`
-    width: fit-content;
-    margin: 0 auto;
-    
+  width: fit-content;
+  margin: 0 auto;
 `
-export const Button = styled.div`
-
-`
+export const Button = styled.div``
 export const Question_button = styled.div`
   background: #505050;
   text-align: center;
@@ -47,37 +41,45 @@ export const Question_button = styled.div`
   font-size: 30px;
   transition: 1s;
   color: white;
-  &:hover{
-  background: rgb(115, 255, 0);
-
+  &:hover {
+    background: rgb(115, 255, 0);
   }
 `
 export const Result_box = styled.div`
-    width: 90%;
-    font-size: 30px;
-    margin: 0 auto;
+  width: 90%;
+  font-size: 30px;
+  margin: 0 auto;
+  position: absolute;
+  text-align : center;
+  top: 40%;
+  
 
-    /* background-color: red; */
-    >div{
-        width: fit-content;
-        margin: 0 auto;
+  @media (max-width: 735px) {
+    div {
+      font-size: 12px;
+      padding: 1%;
     }
+  }
+  > div {
+    width: fit-content;
+    margin: 0 auto;
+  }
 `
 
 export const opacity100 = {
-    opacity : "100%",
-    transitionDuration: "2s"
+  opacity: '100%',
+  transitionDuration: '2s',
 }
 
 export const opacity0 = {
-    opacity : "0%",
-    transitionDuration: "2s"
+  opacity: '0%',
+  transitionDuration: '2s',
 }
 
 export const displayNone = {
-    display : 'none'
+  display: 'none',
 }
 
 export const displayBlock = {
-    display : 'block'
+  display: 'block',
 }
